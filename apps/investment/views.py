d2647e4e589f15493d3d970c3cc4313c8a149cf0
Artem <artem270953@gmail.com>
2023-05-26 14:11:02 +0900

    added routing and templates

--- apps/investment/views.py
+++ apps/investment/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request):
+    return render(request, "pages/home.html", {
+        "page_title": "Главная"
+    })
+
+
+def registration(request):
+    return render(request, "pages/registration.html", {
+        "page_title": "Регистрация"
+    })
+
+
+def login(request):
+    return render(request, "pages/login.html", {
+        "page_title": "Вход"
+    })
+
+
+def logout(request):
+    pass
